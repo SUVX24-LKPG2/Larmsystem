22a6e84c1a246a5b1abf84f2dfd887aeff2e703f
VSilkis <victor.silkisberg@chasacademy.se>
2024-12-09 10:01:17 +0100

    Made a class for customer but need to add a .cpp file for it

--- Customer.h
+++ Customer.h
@@ -4,12 +4,22 @@
 
 class Customer {
 private:
-std::string name;
-std::string username;
-int pincode;
-std::string tagId;
-std::string verificationPhrase;
-
+int customerId;
+std::string customerName;
+std::string customerAdress;
+ 
 public: 
+    Customer(int customerId, std::string customerName,std::string customerAdress) : customerId(customerId), customerName(CustomerName), customerAdress(customerAdress) {}
+   
+   
+    Customer() : customerId(0), customerName(""), customerAdress("") {}
+
+    int getCustomerId() const { return customerId; }
+    std::string getCustomerName() const { return customerName; }
+    std::string getCustomerAddress() const { return customerAddress; }
+
+    void setCustomerId(int id) { customerId = id; }
+    void setCustomerName(const std::string& name) { customerName = name; }
+    void setCustomerAddress(const std::string& address) { customerAddress = address; }
 }
 #endif
