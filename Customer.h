49af9af05f15cdb4c38e09fbf46886ae88ac24df
VSilkis <victor.silkisberg@chasacademy.se>
2024-12-09 10:43:10 +0100

    Fixed a few typing errrors, some where not adressed correctly

--- Customer.h
+++ Customer.h
@@ -9,17 +9,17 @@ std::string customerName;
 std::string customerAdress;
  
 public: 
-    Customer(int customerId, std::string customerName,std::string customerAdress) : customerId(customerId), customerName(CustomerName), customerAdress(customerAdress) {}
+    Customer(int customerId, std::string customerName,std::string customerAdress) : customerId(customerId), customerName(customerName), customerAdress(customerAdress) {}
    
    
     Customer() : customerId(0), customerName(""), customerAdress("") {}
 
     int getCustomerId() const { return customerId; }
     std::string getCustomerName() const { return customerName; }
-    std::string getCustomerAddress() const { return customerAddress; }
+    std::string getCustomerAddress() const { return customerAdress; }
 
     void setCustomerId(int id) { customerId = id; }
     void setCustomerName(const std::string& name) { customerName = name; }
-    void setCustomerAddress(const std::string& address) { customerAddress = address; }
+    void setCustomerAddress(const std::string& address) { customerAdress = address; }
 }
 #endif
