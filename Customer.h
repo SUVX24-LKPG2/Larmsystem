2ae0b487d35a1b136ae1d77cb987507f012733b8
VSilkis <victor.silkisberg@chasacademy.se>
2024-12-09 09:56:47 +0100

    Added a customer class, need to add a .cpp file

---
+++ Customer.h
@@ -0,0 +1,15 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+#include <string>
+
+class Customer {
+private:
+std::string name;
+std::string username;
+int pincode;
+std::string tagId;
+std::string verificationPhrase;
+
+public: 
+}
+#endif
