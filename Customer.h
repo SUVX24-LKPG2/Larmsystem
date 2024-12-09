5531342fe080148d1d4f0a26696b51d9bd8dd577
VSilkis <victor.silkisberg@chasacademy.se>
2024-12-09 11:14:53 +0100

    refactored the code to match

--- Customer.h
+++ Customer.h
@@ -6,20 +6,31 @@ class Customer {
 private:
 int customerId;
 std::string customerName;
-std::string customerAdress;
+std::string adress;
+int pin;
+int tagId;
+std::string verificationPhrase;
  
 public: 
-    Customer(int customerId, std::string customerName,std::string customerAdress) : customerId(customerId), customerName(customerName), customerAdress(customerAdress) {}
+    Customer(int customerId, std::string customerName,std::string adress,int pin, 
+    int tagId, std::string verificationPhrase) : customerId(customerId), customerName(customerName), 
+    adress(adress), pin(pin), tagId(tagId), verificationPhrase(verificationPhrase)  {}
    
    
-    Customer() : customerId(0), customerName(""), customerAdress("") {}
+    Customer() : customerId(0), customerName(""), adress(""), pin(0), tagId(0), verificationPhrase("")  {}
 
-    int getCustomerId() const { return customerId; }
-    std::string getCustomerName() const { return customerName; }
-    std::string getCustomerAddress() const { return customerAdress; }
+    int getId() const { return customerId; }
+    std::string getName() const { return customerName; }
+    std::string getCity() const { return adress; }
+    int getPin() const { return pin; }
+    int getTagId() const { return tagId; }
+    std::string getVerificationPhrase() const { return verificationPhrase; }
 
-    void setCustomerId(int id) { customerId = id; }
-    void setCustomerName(const std::string& name) { customerName = name; }
-    void setCustomerAddress(const std::string& address) { customerAdress = address; }
-}
+    void setId(int id) { customerId = id; }
+    void setName(const std::string& name) { customerName = name; }
+    void setCity(const std::string& city) { adress = city; }
+    void setPin(int pinCode) { pin = pinCode; }
+    void setName(int tag) { tagId = tag; }
+    void setCity(const std::string& phrase) { verificationPhrase = phrase; }
+};
 #endif
