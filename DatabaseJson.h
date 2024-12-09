2ca9ea2a8f5f13997b25b997c2cbb390d47b05e5
Fredrik Storm <fredrik.storm@chasacademy.se>
2024-12-09 10:54:34 +0100

    Add initial jsondb driver

---
+++ DatabaseJson.h
@@ -0,0 +1,23 @@
+#ifndef DB_JSON_H
+#define DB_JSON_H
+
+#include <string>
+
+#include "Database.h"
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+class DatabaseJson : public Database
+{
+public:
+    DatabaseJson(const std::string& db);
+    ~DatabaseJson();
+    void saveCustomer(const Customer& customer) override;
+    void saveComponent(const Component& component) override;
+    void logAlarmEvent(const AlarmEvent& alarmEvent) override;
+    void createEmptyDatabase() override;
+    std::vector<Customer> getCustomers() override;
+};
+
+#endif  // DB_JSON_H
